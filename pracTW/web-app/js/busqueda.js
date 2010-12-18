24ca4f9366972174565fe2c3d66939c020ba5675
arkariang <arkariang@2ae8bb9b-7a8f-4fdf-916e-cbd0066610ee>
2010-12-18 17:15:15 +0000

    Peticion de amistad

--- pracTW/web-app/js/busqueda.js
+++ pracTW/web-app/js/busqueda.js
@@ -3,6 +3,7 @@
  * and open the template in the editor.
  */
 
+var usu=null;
 var find=null;//variable que contendra los datos obtenidos de la busqueda;
 
 function amigosEncontrados()
@@ -14,6 +15,7 @@ function amigosEncontrados()
    //localidad=document.document.getElementById("localidad").value;
    //
    //primero se ha de vacair la tabla
+
    $(".fila").remove();
    $.ajax({
        type:"Get",
@@ -24,7 +26,7 @@ function amigosEncontrados()
            $.each(find,function(i,amigo){
                 //amigo tiene un bool de tieneimagen
                var td1="<tr class=\"fila\"><td style=\"text-align: center\"><img src=\"../images/perfiles/"+amigo.login+"/icono.jpg\" class=\"icono\"></td>"
-               var td2="<td>"+amigo.nombre+"</td><td>"+amigo.apellidos+"</td><td>"+amigo.localizaion+"</td></tr><br>";
+               var td2="<td><a javascript:solicitarAmistad("+amigo.login+")'>"+amigo.nombre+"</a></td><td>"+amigo.apellidos+"</td><td>"+amigo.localizaion+"</td></tr><br>";
                //$(td1+td2).appendTo('busqueda');
                $('#busqueda').append(td1+td2)
             })
@@ -35,11 +37,17 @@ function amigosEncontrados()
 }
 
 
-function rellenaTabla(amigos)
+function solicitarAmistad(idDest)
 {
-    //meter en el html
-    var td1="<tr><td style=\"text-align: center\"><img src=\"../images/perfiles/"+amigos.login+"/icono.jpg\" class=\"icono\"></td>"
-    var td2="<td>"+amigos.nombre+"</td><td>"+amigos.apellidos+"</td><td>"+amigos.localizaion+"</td></tr><br>";
-    $(td1+td2).appendTo('busqueda');
+    alert(idDest);
+    $.ajax({
+        url: "../solicitudes/enviarSolicitud",
+        dataType: "json",
+        data:idDest,
+        success:function(){
+            var p="<p>Peticion realizada</p>";
+            $('#busqueda').append(p)},
+        error: function(){alert('No puede realizarse la peticion'); }
+        })
 
 }
